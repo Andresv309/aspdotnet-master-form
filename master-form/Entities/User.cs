21230c4eb86f96febd8f1ef698fece6e4c0ccbf6
Andresv309 <andresv309@gmail.com>
2023-10-23 08:34:36 -0500

    Add handy entities for dealing with users

---
+++ master-form/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace master_form.Entities
+{
+    public enum UserRole
+    {
+        Admin,
+        Client
+    }
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public UserRole Role { get; set; } = UserRole.Client;
+        public string PhoneNumber { get; set; }
+        public string ProfilePhoto { get; set; } = @"https://t4.ftcdn.net/jpg/05/90/45/35/360_F_590453560_ugMuPncnGYB6XnJqmC8xiPQx4eg3jmMD.jpg";
+
+    }
+}
---
+++ master-form/Entities/UserCredentials.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace master_form.Entities
+{
+    public class UserCredentials
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
