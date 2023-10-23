54d7737c28c47092ec603712c5103b9bc43d6df2
Andresv309 <andresv309@gmail.com>
2023-10-23 08:37:26 -0500

    Add login page

---
+++ master-form/Views/Pages/Login.aspx.cs
@@ -0,0 +1,49 @@
+﻿using master_form.Controllers;
+using master_form.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace master_form.Views.Pages
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!(Session["userRole"] is null))
+            {
+                Response.Redirect("Index.aspx");
+            }
+        }
+
+        protected void loginSubmit_Click(object sender, EventArgs e)
+        {
+            UserCredentials userCredentials = new UserCredentials()
+            {
+                Email = loginEmail.Text.Trim(),
+                Password = loginPassword.Text.Trim(),
+            };
+
+            LoginValidation loginValidation = new LoginValidation();
+            Boolean isUserAuth = loginValidation.ValidateUser(userCredentials);
+
+            if (isUserAuth)
+            {
+                Response.Redirect("Index.aspx");
+            } else
+            {
+                ScriptManager.RegisterStartupScript(this, GetType(), "Invalid Input",
+                    "Swal.fire({" +
+                    "icon: 'error'," +
+                    "title: 'Credenciales Inválidas'," +
+                    "text: 'Email o Contraseña Incorrectos!'})",
+                    true);
+            }
+
+
+        }
+    }
+}
